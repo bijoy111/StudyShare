4c944ed42c46fff04a30f68d9c8e8caed0f98666
bjjoy111 <bijoysaeem@gmail.com>
2023-09-12 23:28:18 +0600

    course module frontend done

--- StudyShare Frontend --v5/button-types.js
+++
@@ -1,9 +0,0 @@
-export const BUTTON_TYPES = {
-    postComment    : "post-comment",
-    postReply      : "post-reply",
-    replyToComment : "reply-to-comment",
-    deleteComment  : "delete-comment",
-    editComment    : "edit-comment",
-    cancelChanges  : "cancel-changes",
-    saveChanges    : "save-changes",
-  };
